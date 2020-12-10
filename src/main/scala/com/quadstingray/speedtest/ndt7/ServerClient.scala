3b16bf3b12096cf66263ace41bd7812a2ad14939
QuadStingray <github@quadstingray.com>
2020-12-10 20:46:42 +0100

    reverted client.serverDetailsBySite

--- src/main/scala/com/quadstingray/speedtest/ndt7/ServerClient.scala
+++ src/main/scala/com/quadstingray/speedtest/ndt7/ServerClient.scala
@@ -4,11 +4,12 @@ import java.net.URI
 
 import com.github.plokhotnyuk.jsoniter_scala.core.{JsonValueCodec, _}
 import com.github.plokhotnyuk.jsoniter_scala.macros._
-import com.quadstingray.speedtest.ndt7.lib.{Server, ServerResult}
+import com.quadstingray.speedtest.ndt7.lib.{Server, ServerDetails, ServerResult}
 
 case class ServerClient() extends HttpClient {
-  implicit private val serverV2codec: JsonValueCodec[Server]                 = JsonCodecMaker.make[Server]
-  implicit private val serverV2Listcodec: JsonValueCodec[ServerResult]       = JsonCodecMaker.make[ServerResult]
+  implicit private val serverCodec: JsonValueCodec[Server]                 = JsonCodecMaker.make[Server]
+  implicit private val serverListCodec: JsonValueCodec[ServerResult]       = JsonCodecMaker.make[ServerResult]
+  implicit private val serverDetailsCodec: JsonValueCodec[List[ServerDetails]] = JsonCodecMaker.make[List[ServerDetails]]
 
   def nextServer: Server = {
     val uri = new URI("https://locate.measurementlab.net/v2/nearest/ndt/ndt7")
@@ -16,4 +17,16 @@ case class ServerClient() extends HttpClient {
     readFromString[ServerResult](response.body().string()).results.head
   }
 
+  def serverDetailsByServer(server : Server) = {
+    serverDetailsBySite(server.siteId)
+  }
+
+  def serverDetailsBySite(siteId: String): Option[ServerDetails] =
+    allServerDetails.find(_.site == siteId)
+
+  def allServerDetails: List[ServerDetails] = {
+    val uri            = new URI("https://siteinfo.mlab-oti.measurementlab.net/v1/sites/locations.json")
+    val response = httpClient().newCall(buildRequest(uri)).execute()
+    readFromString[List[ServerDetails]](response.body().string())
+  }
 }
